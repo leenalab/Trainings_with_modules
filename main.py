b13a410901f2f9afed81da005f919a571167d34c
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-20 02:22:20 +0300

    duumy_functions

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from calculation import salary_calculations
+from calculation.salary_calculations import add_bonus
 
 salary = 1000
 bonus = 15
-salary_with_bonus = salary_calculations.add_bonus(salary, bonus)
+salary_with_bonus = add_bonus(salary, bonus)
 print(salary_with_bonus)  # 1015
+
+
 
 
 
