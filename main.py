dc7b4fe505ab0727623d9b4155c63b6d340c69c2
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-20 01:19:39 +0300

    Change_module

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ print(say_hello("World"))
 from mymodule import say_hello as greeting
 
 print(greeting("World"))
+
+from mymodule import say_hello as greeting
+
+print(dir())
+print(greeting("World"))
