6a91718e988f05a9fe0d184b73b2dafade5aa52b
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-21 22:28:01 +0300

    trainings_counter

---
+++ counter_name_tupple.py
@@ -0,0 +1,31 @@
+# Приклад:
+"""
+student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7, 1, 1, 1, 3, 5]
+mark_counts = {}
+for mark in student_marks:
+    if mark in mark_counts:
+        mark_counts[mark] += 1
+    else:
+        mark_counts[mark] = 1
+
+print(mark_counts)
+"""
+import collections
+
+student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7 , 1, 1, 1, 3, 5]
+mark_counts = collections.Counter(student_marks)
+#print(mark_counts)
+
+
+ # один з методів Counter - це most_common()
+
+#print(mark_counts.most_common())
+# print(mark_counts.most_common(1))
+# print(mark_counts.most_common(2))
+# print(mark_counts.most_common(3))
+
+from collections import Counter
+
+# Створення Counter з рядка
+letter_count = Counter("To be or not to be, that is the question.")
+print(letter_count)
