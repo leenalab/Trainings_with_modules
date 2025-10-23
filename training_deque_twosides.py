f8653ac595a8c0126a831f700ccb45936fdde45a
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-23 22:32:52 +0300

    que?tasks

--- training_deque_twosides.py
+++ training_deque_twosides.py
@@ -1,5 +1,7 @@
+
 from collections import deque
 
+"""
 # Створення пустої двосторонньої черги
 d = deque()
 
@@ -19,3 +21,31 @@ print("Видалений перший елемент:", d.popleft())
 
 # Виведення поточного стану черги після видалення елементів
 print("Черга після видалення елементів:", list(d))
+"""
+
+from collections import deque
+
+# Список завдань, де кожне завдання - це словник
+tasks = [
+    {"type": "fast", "name": "Помити посуд"},
+    {"type": "slow", "name": "Подивитись серіал"},
+    {"type": "fast", "name": "Вигуляти собаку"},
+    {"type": "slow", "name": "Почитати книгу"}
+]
+
+# Ініціалізація черги завдань
+task_queue = deque()
+
+# Розподіл завдань у чергу відповідно до їх пріоритету
+for task in tasks:
+    if task["type"] == "fast":
+        task_queue.appendleft(task)  # Додавання на високий пріоритет
+        print(f"Додано швидке завдання: {task['name']}")
+    else:
+        task_queue.append(task)  # Додавання на низький пріоритет
+        print(f"Додано повільне завдання: {task['name']}")
+
+# Виконання завдань
+while task_queue:
+    task = task_queue.popleft()
+    print(f"Виконується завдання: {task['name']}")
